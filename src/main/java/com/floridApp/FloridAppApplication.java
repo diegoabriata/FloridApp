66bc5ddf2353f186f2f4b7a97effc462fcf4ed4e
Diego <diego.abriata@gmail.com>
2018-07-17 15:14:27 -0300

    First Commit

---
+++ src/main/java/com/floridApp/FloridAppApplication.java
@@ -0,0 +1,12 @@
+package com.floridApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloridAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloridAppApplication.class, args);
+	}
+}
